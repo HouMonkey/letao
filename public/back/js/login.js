e0e9e10fd7b1368de09b73ac1396b397fbc15565
hzy <111111@qq.com>
2018-01-13 03:20:23 +0800

    登录功能完成

--- public/back/js/login.js
+++ public/back/js/login.js
@@ -5,7 +5,7 @@ $(function () {
 
   //要求用户名不能为空
   //要求密码不能为空，并且长度是6-12
-  //表单校验插件，在表单提交的时候做校验如果校验失败了，会阻止表单的提交。如果校验成功了，它就会让表单继续提交。
+  //表单校验插件，在表单提交的时候做，校验如果校验失败了，会阻止表单的提交。如果校验成功了，它就会让表单继续提交。
 
   var $form = $("form");
   //初始化表单校验插件
@@ -56,5 +56,54 @@ $(function () {
         }
       }
     }
+  });
+
+  //需要给表单注册一个校验的事件，这个事件在表单校验成功的时候会触发，success.form.bv
+  $form.on("success.form.bv" , function (e) {
+    //阻止表单提交
+    e.preventDefault();
+
+    //发送ajax请求
+    //console.log("哈哈哈");
+    $.ajax({
+      type:"post",
+      url:"/employee/employeeLogin",
+      dataType:"json",//(这里可写可不写)
+      //注意：dataType:jquery会根据后端返回的类型自动识别。
+      //content-type:text/json  text/html
+      data:$form.serialize(),
+      success:function (info) {
+        //console.log(info);
+
+        //登陆成功
+        if (info.success) {
+          //跳转到index页面
+          location.href = "index.html";
+        }
+
+        //失败
+        if (info.error == 1000) {
+          //alert(info.message);
+          //使用updateStatus方法，把用户名改成失败即可
+          //$form.data("bootstrapValidator")用于获取插件实例，通过这个实例可以调用方法
+          //3个参数：
+          //1.字段名--->如：username
+          //2.状态：VALID INVALID
+          //3.显示哪个校验的内容
+          $form.data("bootstrapValidator").updateStatus("username" , "INVALID" , "callback");
+        }
+        if (info.error == 1001) {
+          //alert(info.message);
+          //手动把password改成INVALID
+          $form.data("bootstrapValidator").updateStatus("password" , "INVALID" , "callback");
+        }
+      }
+    });
+  })
+
+  //给reset按钮注册一个点击事件
+  $("[type='reset']").on("click" , function () {
+    //重置校验的样式
+    $form.data("bootstrapValidator").resetForm();
   });
 });
