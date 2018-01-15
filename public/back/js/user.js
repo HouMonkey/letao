7f663adf2ff3b55224490764c8c74cab89d63b68
hzy <111111@qq.com>
2018-01-15 16:01:03 +0800

    用户管理模块完成

--- public/back/js/user.js
+++ public/back/js/user.js
@@ -56,6 +56,47 @@ $(function () {
   }
 
 
+  //用户启用禁用的功能
+  //1.给禁用或者启用注册点击事件，需要注册委托事件
+  //2.弹出模块框
+  $('tbody').on('click' , '.btn' , function () {
+    //console.log('嘿嘿嘿');
 
+    //让启用禁用模态框显示
+    $('#userModal').modal('show');
 
-})
+    //获取到id
+    var id = $(this).parent().data('id');
+    //获取到是否禁用 如果是btn-success类，说明需要启用这个用户，需要传1
+    var isDelete = $(this).hasClass('btn-success') ? 1 : 0;
+
+    //console.log(id , isDelete);
+
+    $('.btn_confirm').off().on('click' , function () {
+
+      //console.log('啦啦啦');
+
+      $.ajax({
+        type: 'post',
+        url: '/user/updateUser',
+        data: {
+          id:id,
+          isDelete:isDelete
+        },
+        success: function (info) {
+
+          //console.log(info);
+
+          if (info.success) {
+            //关闭模态框
+            $('#userModal').modal('hide');
+
+            //重新渲染
+            render();
+          }
+        }
+      });
+    });
+  });
+
+});
